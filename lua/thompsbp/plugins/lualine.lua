e54f3943d42f917bce112a219fcf024f84887735
Brayton Thompson <thompsbp@globalnoc.iu.edu>
2025-03-16 23:19:00 -0400

    Added LSP information to lualine

--- lua/thompsbp/plugins/lualine.lua
+++ lua/thompsbp/plugins/lualine.lua
@@ -54,8 +54,25 @@ return {
 			options = {
 				theme = my_lualine_theme,
 			},
+			extensions = { "nvim-tree" },
 			sections = {
 				lualine_x = {
+					{
+						"lsp_status",
+						icon = "", -- f013
+						symbols = {
+							-- Standard unicode symbols to cycle through for LSP progress:
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							-- Standard unicode symbol for when LSP is done:
+							done = "✓",
+							-- Delimiter inserted between LSP names:
+							separator = " ",
+						},
+						-- List of LSP names to ignore (e.g., `null-ls`):
+						ignore_lsp = {},
+					},
+				},
+				lualine_y = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
